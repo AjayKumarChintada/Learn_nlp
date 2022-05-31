00f391c123ce7d12a782cc0c8d6e5307922bb77d
Ajay <ajay@Ajays-MacBook-Pro.local>
2022-05-31 16:10:28 +0530

    processed notebook

--- classify_spam.py
+++ classify_spam.py
@@ -6,12 +6,7 @@ from preprocessing import process_text
 df = pd.read_csv('spam.csv', encoding="ISO-8859-1")
 df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 df.rename(columns={'v1': 'Label', 'v2': 'Mail'}, inplace=True)
-
 print(df['Label'].value_counts())
 
 df['Label'] = df['Label'].map({'ham': 1, 'spam': 0})
-
-
 df['msg'] = df['Mail'].apply(lambda x: process_text(x))
-print(df.head())
-
